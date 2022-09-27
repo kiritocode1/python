0a7fd38695536badffc6f661c87f854ebc36cd6b
kiritocode1 <kathawalearyan9@gmail.com>
2022-09-27 15:32:11 +0530

    Commit Safe : Started with python blog

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import { Scroll } from "scrollex";
+
 import "./style.css"
-
-const keyframes = {
-  heading: ({ section, container }) => ({
-    [section.topAt("container-bottom")]: {
-      translateX: -container.width,
-    },
-    [section.topAt("container-top") - container.height / 4]: {
-      translateX: 0,
-    },
-    [section.topAt("container-top") + container.height / 4]: {
-      translateX: 0,
-    },
-    [section.bottomAt("container-top")]: {
-      translateX: container.width,
-    },
-  }),
-};
-
-
-
-
+import { Image, Text } from "@nextui-org/react";
+import { Progress } from "@nextui-org/react";
 
 export default function App() {
   return (
-    <Scroll.Container scrollAxis="y" className="h-screen">
-      <Scroll.Section className="h-screen center bg-1">
-        <Scroll.Item keyframes={keyframes.heading}>
-          <h1>Blog Coming Soon</h1>
-        </Scroll.Item>
-      </Scroll.Section>
-      <Scroll.Section className="h-screen center bg-2">
-        <Scroll.Item keyframes={keyframes.heading}>
-          <h1>Dont Worry</h1>
-        </Scroll.Item>
-      </Scroll.Section>
-      <Scroll.Section className="h-screen center bg-1">
-        <Scroll.Item keyframes={keyframes.heading}>
-          <h1>I am fast <br/> ◉/_\◉</h1>
-        </Scroll.Item>
-      </Scroll.Section>
-    </Scroll.Container>
+    <div className='w-full flex items-center justify-evenly my-20 gap-4 flex-col'>
+      <Image src="https://c.tenor.com/qnp4xGGoC44AAAAC/cute-awe.gif" width={450} />
+      <Text h1 size={60} css={{ textGradient: "45deg ,$green50 -30% , $green600 90%" }} >Blog #2 Python:Basics 🐍  </Text>
+      <Text color="$green800" >Hello World , its me Aryan back with another blog about basics of Python 🐍 Programming language , <br />now this might be blog 2 but cannonically this is the first blog im writing ,<br /> this blog is beginners friendly 😊 and anyone can read this blog to understand a lot about <br /> python as a programming language . <br /> in the future 🚀 blog we'll cover python with data science    </Text>
+      <div className="w-96 flex flex-col">
+        <Text h4 color="$green800" weight={70}>Reader Level : 🐣 </Text>
+        <Progress value={5} shadow color="success" status="success"  size="xl"/>
+      </div>
+      <Text h1 size={69} color="$green800">Work In Progress 💀</Text>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 1. Import `createTheme`
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+
+// 2. Call `createTheme` and pass your custom values
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      // you can also create your own color
+      myColor: '#ff4ecd'
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider theme={theme}><App/></NextUIProvider>
   </React.StrictMode>
 );
 
